5bfb9cbc9e06dff41077a1a1ef017a78ab4ab4e9
Wesley Workman <wworkman@batterii.com>
2017-11-17 14:15:02 -0500

    Scrubbed down to the 1 deploy.

---
+++ Step-Instructions.md
@@ -0,0 +1,11 @@
+# Step 1
+
+## Enable App Engine
+
+`gcloud app create`
+
+## Enable the Datastore API
+
+https://console.cloud.google.com/datastore/entities/query
+
+`gcloud service-management enable datastore.googleapis.com`
--- src/main/java/io/workmanw/yapa/serializers/RestSerializer.java
+++ src/main/java/io/workmanw/yapa/serializers/RestSerializer.java
@@ -1,22 +1,18 @@
 package io.workmanw.yapa.serializers;
 
 import io.workmanw.yapa.models.AlbumModel;
-import io.workmanw.yapa.models.PhotoModel;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import java.util.List;
 import java.util.ArrayList;
 
 public class RestSerializer {
   protected List<AlbumModel> albums;
-  protected List<PhotoModel> photos;
 
   public RestSerializer() {
     this.albums = new ArrayList<AlbumModel>();
-    this.photos = new ArrayList<PhotoModel>();
   }
 
   public RestSerializer addAlbum(AlbumModel album) {
@@ -26,16 +22,6 @@ public class RestSerializer {
 
   public RestSerializer addAlbums(List<AlbumModel> albums) {
     this.albums.addAll(albums);
-    return this;
-  }
-
-  public RestSerializer addPhoto(PhotoModel photo) {
-    this.photos.add(photo);
-    return this;
-  }
-
-  public RestSerializer addPhotos(List<PhotoModel> photos) {
-    this.photos.addAll(photos);
     return this;
   }
 
@@ -49,18 +35,9 @@ public class RestSerializer {
       }
       ret.add("album", albumsJson);
     }
-    if (this.photos.size() > 0) {
-      JsonArray photosJson = new JsonArray();
-      for (PhotoModel photo : this.photos) {
-        photosJson.add(photo.toJson());
-      }
-      ret.add("photo", photosJson);
-    }
 
     return ret;
   }
-
-  // protected JsonArray _serializeEntities(BaseModel m,)
 
   public String toString() {
     return this.toJson().toString();
